f4934d2ccbc65982d2f5f9c9a36a688780e9ddc4
SassafrasYellow <enderfighter99@gmail.com>
2026-05-04 21:44:37 -0400

    Contact Book
    Now shows contacts and a small contact book has been made

---
+++ ContactBookCSharp/ContactBook.cs
@@ -0,0 +1,157 @@
+namespace ContactBook;
+
+public class ContactBook
+{
+    public const string NEXT_PAGE = "+";
+    public const string PREV_PAGE = "-";
+    public const string GOTO_PAGE = "G";
+    public const string PAGE_SIZE = "S";
+    public const string CREATE_CONTACT = "C";
+    public const string REVIEW_CONTACT = "R";
+    public const string UPDATE_CONTACT = "U";
+    public const string DELETE_CONTACT = "D";
+    public const string FIND_CONTACT = "F";
+    public const string ORDER_CONTACTS = "O";
+    public const string DEDUPLICATE_CONTACTS = "M";
+    public const string EXIT = "X";
+
+    public readonly string[] COMMANDS = new string[]
+    {
+     NEXT_PAGE,
+     PREV_PAGE,
+     GOTO_PAGE,
+     PAGE_SIZE,
+     CREATE_CONTACT,
+     REVIEW_CONTACT,
+     UPDATE_CONTACT,
+     DELETE_CONTACT,
+     FIND_CONTACT,
+     ORDER_CONTACTS,
+     DEDUPLICATE_CONTACTS,
+     EXIT
+    };
+    private List<Contact> allContacts;
+    public ContactBook(List<Contact> contacts = null!)
+    {
+        allContacts = (contacts == null) ? new List<Contact>() : contacts;
+    }
+
+    public void Start()
+    {
+        ShowWelcomeScreen();
+
+string input;
+        do
+        {
+            ShowContacts();
+        
+            do
+            {
+                ShowInputOptions();
+                input = GetInput();
+            }
+            while(!IsValidInput(input));
+
+            ProcessInput(input);
+        }
+        while(!ConfirmExit());
+
+        ShowExitScreen();
+         
+
+    }
+
+ private void ShowWelcomeScreen()
+    {
+       Console.WriteLine("Welcome to Jorge's Contact Book!");
+       PressEnterContinue();
+    }
+       private void ShowContacts()
+    {
+        Console.Clear();
+       if(allContacts.Count <= 0)
+        {
+            Console.WriteLine("No contacts to show.");
+        }
+        else
+        {
+            int indexCol = Math.Max("#".Length, allContacts.Count.ToString().Length);
+            int fnameCol = Math.Max("First Name".Length, allContacts.Max(c => c.GetFName()?.Length ?? 0));
+            int lnameCol = Math.Max("Last Name".Length, allContacts.Max(c => c.GetLName()?.Length ?? 0));
+            int phoneCol = Math.Max("Phone".Length, allContacts.Max(c => c.GetPhone()?.Length ?? 0));
+            int emailCol = Math.Max("Email".Length, allContacts.Max(c => c.GetEmail()?.Length ?? 0));
+
+            Console.WriteLine(""
+                + "{0," + -indexCol + "}   "
+                + "{1," + -fnameCol + "}   "
+                + "{2," + -lnameCol + "}   "
+                + "{3," + -phoneCol + "}   "
+                + "{4," + -emailCol + "}   ",
+                "#", "First Name", "Last Name", "Phone", "Email");
+                Console.WriteLine(new string('~',indexCol + 2 + fnameCol + 2 + lnameCol + 2 + phoneCol + 2 + emailCol));
+
+            int n = allContacts.Count;
+            int page = 1;
+            int size = 10;
+            int pageCount = (int) Math.Max(1, Math.Ceiling(n/ (double) size));
+            int s = Math.Clamp((page-1) * size, 0, n);
+            int e = Math.Clamp(s + size, 0, n);
+
+            for(int i = s; i < e; i++)
+            {
+                Contact c = allContacts[i];
+
+                Console.WriteLine(""
+                + "{0," + -indexCol + "}   "
+                + "{1," + -fnameCol + "}   "
+                + "{2," + -lnameCol + "}   "
+                + "{3," + -phoneCol + "}   "
+                + "{4," + -emailCol + "}   ",
+                (i+1), c.GetFName(), c.GetLName(), c.GetPhone(), c.GetEmail());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Page {page} of {pageCount} ({s}-{e} of {n})");
+        }
+    }
+
+
+    private string GetInput()
+    {
+        return Console.ReadLine() ?? string.Empty;
+    }
+    
+    private void ShowExitScreen()
+    {
+        
+    }
+
+    private bool ConfirmExit()
+    {
+       return true;
+    }
+
+    private void ProcessInput(string input)
+    {
+        
+    }
+
+    private bool IsValidInput(string input)
+    {
+        return true;
+    }
+
+ 
+   
+    private void ShowInputOptions()
+    {
+        
+    }
+
+    private void PressEnterContinue()
+    {
+        Console.WriteLine("Press ENTER to continue...");
+        Console.ReadLine();
+        
+    }
+}
